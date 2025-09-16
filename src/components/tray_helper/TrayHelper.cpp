45b21bdc6c1c620fd4f1d36f9f9316ff741e90c1
EDanilchenko <Danilchenko@outlook.com>
2025-09-16 10:12:52 +0200

    add header to tray

--- src/components/tray_helper/TrayHelper.cpp
+++ src/components/tray_helper/TrayHelper.cpp
@@ -1,30 +1,63 @@
 #include "TrayHelper.h"
 #include <QWidgetAction>
 #include <QApplication>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPixmap>
+#include <QIcon>
+#include <QPalette>
 
 namespace TrayHelper {
-    void setupTray(MainWindow* window, const QIcon& icon) {
-        if (!QSystemTrayIcon::isSystemTrayAvailable()) return;
 
-        auto* state = window->getAppState();
+void setupTray(MainWindow* window, const QIcon& icon) {
+    if (!QSystemTrayIcon::isSystemTrayAvailable()) return;
 
-        auto* trayIcon = new QSystemTrayIcon(window);
-        trayIcon->setIcon(icon);
+    auto* state = window->getAppState();
 
-        auto* trayMenu = new QMenu(window);
+    auto* trayIcon = new QSystemTrayIcon(window);
+    trayIcon->setIcon(icon);
 
-        auto* trayPanel = new ControlPanel(window);
-        trayPanel->setState(state);
+    auto* trayMenu = new QMenu(window);
 
-        auto* widgetAction = new QWidgetAction(trayMenu);
-        widgetAction->setDefaultWidget(trayPanel);
-        trayMenu->addAction(widgetAction);
+    auto* headerWidget = new QWidget(trayMenu);
+    headerWidget->setAutoFillBackground(true);
+    QPalette pal = headerWidget->palette();
+    pal.setColor(QPalette::Window, Qt::white);
+    headerWidget->setPalette(pal);
 
-        auto* quitAction = new QAction("Exit", window);
-        QObject::connect(quitAction, &QAction::triggered, qApp, &QApplication::quit);
-        trayMenu->addAction(quitAction);
+    auto* headerLayout = new QHBoxLayout(headerWidget);
+    headerLayout->setContentsMargins(10, 5, 10, 5);
+    headerLayout->setSpacing(10);
 
-        trayIcon->setContextMenu(trayMenu);
-        trayIcon->show();
-    }
+    auto* appIconLabel = new QLabel(headerWidget);
+    appIconLabel->setPixmap(icon.pixmap(48, 48));
+    headerLayout->addWidget(appIconLabel, 0, Qt::AlignVCenter);
+
+    auto* titleLabel = new QLabel("Work Time Tracker", headerWidget);
+    QFont font = titleLabel->font();
+    font.setPointSize(14);
+    font.setBold(true);
+    titleLabel->setFont(font);
+    titleLabel->setAlignment(Qt::AlignVCenter | Qt::AlignHCenter);
+    headerLayout->addWidget(titleLabel, 1);
+
+    auto* headerAction = new QWidgetAction(trayMenu);
+    headerAction->setDefaultWidget(headerWidget);
+    trayMenu->addAction(headerAction);
+
+    auto* trayPanel = new ControlPanel(window);
+    trayPanel->setState(state);
+
+    auto* widgetAction = new QWidgetAction(trayMenu);
+    widgetAction->setDefaultWidget(trayPanel);
+    trayMenu->addAction(widgetAction);
+
+    auto* quitAction = new QAction("Exit", window);
+    QObject::connect(quitAction, &QAction::triggered, qApp, &QApplication::quit);
+    trayMenu->addAction(quitAction);
+
+    trayIcon->setContextMenu(trayMenu);
+    trayIcon->show();
+}
+
 }
